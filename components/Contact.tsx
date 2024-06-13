4673821064c13554688c465cf9b7ff382bedf5fc
ta-02 <talal.gl2004@gmail.com>
2024-06-13 19:28:49 -0400

    Adding email support

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./SectionHeading";
+import { motion } from "framer-motion";
+import useSectionInView from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submitBtn";
+import toast from "react-hot-toast";
+
 const Contact = () => {
   return <div></div>;
 };
